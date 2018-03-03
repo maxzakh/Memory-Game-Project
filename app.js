c0cca8d53781e3e65619da12ec1e50b9c31c44bb
Maxzakh <maxzakh@ucdavis.edu>
2018-03-03 01:44:34 -0800

    Almost completed project

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ function indexToCard(idx) {
     return document.querySelector("#tile_" + idx);
 }
 
-function updateStars() {
+function amountOfStars() {
     let nStars = 0;
     let limit = 4;
     if (moveCounter <= limit) {
@@ -46,16 +46,65 @@ function updateStars() {
     else if (moveCounter <= limit + 20) {
         nStars = 1;
     }
-    let starElements = document.querySelectorAll(".fa.fa-star");
-    starElements[0].style.visibility = nStars >= 1 ? "" : "hidden";
-    starElements[1].style.visibility = nStars >= 2 ? "" : "hidden";
-    starElements[2].style.visibility = nStars >= 3 ? "" : "hidden";
+    return nStars;
 }
 
-function cardClick() {
-    this.classList.add("show");
+function updateStars() {
+    let nStars = amountOfStars();
+    let starElements = document.querySelectorAll(".fa.fa-star");
+    starElements[0].style.opacity = nStars >= 1 ? "1" : "0.1";
+    starElements[1].style.opacity = nStars >= 2 ? "1" : "0.1";
+    starElements[2].style.opacity = nStars >= 3 ? "1" : "0.1";
+}
+
+let openCards = [];
+
+function getCurrentClass(el) {
+    return el.classList[2];
+}
+
+function cardClick() {  
+    let currentIndex = extractTileId(this.id);
+    console.log(currentIndex);
+
+    if (openCards.length === 0) {
+        openCards.push(currentIndex);
+        this.classList.add("show", "open");
+    } else if (openCards.length === 1) {
+        if (openCards[0] === currentIndex) {
+            return;
+        }
+        openCards.push(currentIndex);
+        this.classList.add("show", "open");
+
+        let cardEl1 = indexToCard(openCards[0]);
+        let cardEl2 = indexToCard(openCards[1]);
+
+        let card1 = getCurrentClass(cardEl1);
+        let card2 = getCurrentClass(cardEl2);
+        if (card1 === card2) {
+            cardEl1.classList.add("match");
+            cardEl2.classList.add("match");
+            cardEl1.classList.remove("open");
+            cardEl2.classList.remove("open");
+            openCards = [];
+        } else {
+            setTimeout(function resetTwoCards() {
+                cardEl1.classList.remove("show", "open");
+                cardEl2.classList.remove("show", "open");
+                openCards = [];
+            }, 700);
+       }
+    }
     setMoveCounter(moveCounter + 1);
     updateStars();
+
+    let isOver = document.querySelectorAll(".match").length === DECK_SIZE;
+    if (isOver) {
+        stopTimer();
+        showWinMessage();
+        startTimer();
+    }
 }
 
 let counter = 0;
@@ -77,7 +126,14 @@ function stopTimer() {
 
 function onGameTimer() {
     counter++;
-    counterElement.innerHTML = "" + counter;
+    counterElement.innerHTML = formatElapsedTime(counter);
+}
+
+function formatElapsedTime(time) {
+    //From Stack Overflow
+    var date = new Date(null);
+    date.setSeconds(time);
+    return date.toISOString().substr(11, 8);
 }
 
 function initDomElements(size, parent) {
@@ -91,6 +147,7 @@ function initDomElements(size, parent) {
 }
 
 function assignTileClasses(tiles, cssClasses) {
+    //Go through all tiles and set classes associated with current tile value
     for (let i = 0; i < tiles.length; i++) {
         let el = indexToCard(i);
         let cls = cssClasses[tiles[i]];
@@ -98,15 +155,33 @@ function assignTileClasses(tiles, cssClasses) {
     }
 }
 
+function restartGame() {
+    stopTimer();
+    counter = 0;
+    counterElement.innerHTML = formatElapsedTime(counter);
+    setMoveCounter(0);
+    updateStars();
+    openCards = [];
+
+    //Shuffle tiles and reassign CSS classes back to tiles
+    shuffle(tiles);
+    tiles.forEach((tileIndex) => {
+        let cardElement = indexToCard(tileIndex);
+        cardElement.className = "card fa";
+        let cls = cssClasses[tiles[tileIndex]];
+        cardElement.classList.add(cls);
+    });
+    startTimer();
+}
+
 function restartClick() {
+    //Check if game is running
     if (counterIntervalId) {
-        stopTimer();
-        counter = 0;
-        counterElement.innerHTML = "" + counter;
-        setMoveCounter(0);
-        updateStars();
+        //Reset to initial state if game is running
+        restartGame();
     }
     else {
+        //If game is not running then start the game timer
         startTimer();
     }
 }
@@ -114,6 +189,24 @@ function restartClick() {
 function setMoveCounter(value) {
     moveCounter = value;
     document.querySelector(".moves").innerHTML = "" + moveCounter;
+}
+
+function showPlural(num, text) {
+    if (num === 1) {
+        return `${num} ${text}`;
+    } else {
+        return `${num} ${text}s`;
+    }
+}
+
+function showWinMessage() {
+    let stars = amountOfStars();
+    let msg = `Congratulations, you won!
+    It took you ${moveCounter} moves to win.
+    You won with ${showPlural(stars, "star")} in ${showPlural(counter, "second")}.
+    
+    Would you like to play again?`;
+    alert(msg);
 }
 
 function main() {
